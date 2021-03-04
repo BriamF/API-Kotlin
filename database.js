0fef7a7c4bd0b3339e482e176af66cad3280f9b5
BriamF <briam.flores11@gmail.com>
2021-03-04 01:50:22 -0600

    Add config database CREATE POOL 2.0

--- database.js
+++ database.js
@@ -9,8 +9,7 @@ const mysqlConnection = mysql.createPool({
 
 //bee8020b1a1e24:686316dc@us-cdbr-east-03.cleardb.com/heroku_079683f81f34309
 
-
-mysqlConnection.connect(function(e){
+mysqlConnection.acquireConnection(function(e){
     if(e){
         console.log(e);
         return;
@@ -18,6 +17,7 @@ mysqlConnection.connect(function(e){
         console.log('DB is connected');
     }
 });
+
 
 
 
