979a2a828c269bd087401c2929fe69f3eca2bedf
BriamF <briam.flores11@gmail.com>
2021-03-04 01:40:05 -0600

    Add config database ERROR LOST CONNECTION

--- database.js
+++ database.js
@@ -1,17 +1,23 @@
 const mysql = require('mysql');
 
+/*
 const mysqlConnection = mysql.createConnection({
     host: 'us-cdbr-east-03.cleardb.com',
     user: 'bee8020b1a1e24',
     password: '686316dc',
     database: 'heroku_079683f81f34309'
 });
+*/
 
 //bee8020b1a1e24:686316dc@us-cdbr-east-03.cleardb.com/heroku_079683f81f34309
+const dburl = process.env.MYSQL_ADDON 
+    ||   'mysql://' + process.env.DB_USER||'bee8020b1a1e24'+ ':' + process.env.DB_PASS||'686316dc'
+        + '@localhost/' + process.env.DB_NAME||'heroku_079683f81f34309';
 
+const conn = mysql.createConnection(dburl);
 
 function handleDisconnect() {
-    connection = mysql.createConnection(mysqlConnection); 
+    connection = mysql.createConnection(conn); 
 
     connection.connect(function(err) {              
         if(err) {                                     
@@ -29,9 +35,8 @@ function handleDisconnect() {
     });
 }
 
-setInterval(function () {
-    handleDisconnect(); 
-}, 5000);
+handleDisconnect(); 
+
 
 
 
